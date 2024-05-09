abce9e94c62b2f4906032697e8951afac932295e
kiv1n <aloha@kiv1n.ru>
2024-05-09 17:34:16 +0300

    Implement explore page

---
+++ app/components/token-explore-list.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { SiteConfigContracts } from "@/config/site";
+import { agroTokenAbi } from "@/contracts/abi/agroToken";
+import { useEffect, useState } from "react";
+import { useReadContract } from "wagmi";
+import EntityList from "./entity-list";
+import { TokenCard } from "./token-card";
+
+export function TokenExploreList(props: { contracts: SiteConfigContracts }) {
+  const [tokens, setTokens] = useState<string[] | undefined>();
+
+  const { data: nextTokenId } = useReadContract({
+    address: props.contracts.agroToken,
+    abi: agroTokenAbi,
+    functionName: "getNextTokenId",
+    chainId: props.contracts.chain.id,
+  });
+
+  useEffect(() => {
+    if (nextTokenId) {
+      const tokens = Array.from({ length: Number(nextTokenId) }, (_, i) =>
+        i.toString()
+      );
+      setTokens(tokens);
+    }
+  }, [nextTokenId]);
+
+  return (
+    <EntityList
+      entities={tokens}
+      renderEntityCard={(token, index) => (
+        <TokenCard
+          key={index}
+          token={token}
+          contracts={props.contracts}
+          returnActionVisible={true}
+          addRecordActionVisible={true}
+        />
+      )}
+      noEntitiesText={`No tokens on ${props.contracts.chain.name} 😐`}
+      className="gap-6"
+    />
+  );
+}
