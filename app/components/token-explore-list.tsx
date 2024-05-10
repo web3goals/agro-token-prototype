aeb835f5e19c665757ebe9ff6588f728e51c0100
kiv1n <aloha@kiv1n.ru>
2024-05-10 17:29:31 +0300

    Display only available tokens in explore page

--- app/components/token-explore-list.tsx
+++ app/components/token-explore-list.tsx
@@ -3,28 +3,58 @@
 import { SiteConfigContracts } from "@/config/site";
 import { agroTokenAbi } from "@/contracts/abi/agroToken";
 import { useEffect, useState } from "react";
-import { useReadContract } from "wagmi";
+import { isAddressEqual, zeroAddress } from "viem";
+import { useInfiniteReadContracts } from "wagmi";
 import EntityList from "./entity-list";
 import { TokenCard } from "./token-card";
+
+const LIMIT = 42;
 
 export function TokenExploreList(props: { contracts: SiteConfigContracts }) {
   const [tokens, setTokens] = useState<string[] | undefined>();
 
-  const { data: nextTokenId } = useReadContract({
-    address: props.contracts.agroToken,
-    abi: agroTokenAbi,
-    functionName: "getNextTokenId",
-    chainId: props.contracts.chain.id,
+  const { data } = useInfiniteReadContracts({
+    cacheKey: `explore_list_${props.contracts.chain.id.toString()}`,
+    contracts(pageParam) {
+      return [...new Array(LIMIT)].map(
+        (_, i) =>
+          ({
+            address: props.contracts.agroToken,
+            abi: agroTokenAbi,
+            functionName: "getParams",
+            args: [BigInt(pageParam + i)],
+            chainId: props.contracts.chain.id,
+          } as const)
+      );
+    },
+    query: {
+      initialPageParam: 0,
+      getNextPageParam: (_lastPage, _allPages, lastPageParam) => {
+        return lastPageParam + 1;
+      },
+    },
   });
 
   useEffect(() => {
-    if (nextTokenId) {
-      const tokens = Array.from({ length: Number(nextTokenId) }, (_, i) =>
-        i.toString()
-      );
+    setTokens(undefined);
+    if (data) {
+      const tokens: string[] = [];
+      const dataFirstPage = (data as any).pages[0];
+      for (let i = 0; i < dataFirstPage.length; i++) {
+        const dataPageElement = dataFirstPage[i];
+        if (
+          dataPageElement.result.investmentAmount.toString() !== "0" &&
+          isAddressEqual(
+            dataPageElement.result.investor || zeroAddress,
+            zeroAddress
+          )
+        ) {
+          tokens.push(String(i));
+        }
+      }
       setTokens(tokens);
     }
-  }, [nextTokenId]);
+  }, [data]);
 
   return (
     <EntityList
